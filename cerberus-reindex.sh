0877be86f7114edd849cf26b5ff05c3100b8ba28
ViniciusBitnoob <46408861+ViniciusBitnoob@users.noreply.github.com>
2019-01-11 20:05:14 -0200

    Update cerberus-reindex.sh

--- cerberus-reindex.sh
+++ cerberus-reindex.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 cd
-echo "Bem vindo ao Scrypts da Bitnoob"
-echo 
-echo "Será iniciado a Reatualização dos blocos da wallet de Cerberus"
+echo "BEM VINDO AOS SCRYPTS DO CANAL BITNOOB!!!"
+sleep 3
+echo "Será iniciado a Reatualização dos blocos da wallet de Cerberus!"
 
+echo "Pausando a wallet..."
+sleep 3
 cd 
 cd cerberus_120103_linux
 ./cerberus-cli stop
-sleep 3
+sleep 5
+echo "Reiniciando sincronização dos Blocos...!
 ./cerberusd -reindex
-sleep 3
+sleep 5
 ./cerberus-cli mnsync status
 echo "Se os parametros foram true, pode iniciar o novamente Masternode"
 echo "Caso não for true, aguarde mais alguns minutos e use os comandos"
