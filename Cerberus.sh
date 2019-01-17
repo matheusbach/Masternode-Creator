19d70e5f0dd4d5c18753a657b2a04cb1e031c719
ViniciusBitnoob <46408861+ViniciusBitnoob@users.noreply.github.com>
2019-01-17 17:00:40 -0200

    Update Cerberus.sh

--- Cerberus.sh
+++ Cerberus.sh
@@ -30,8 +30,10 @@ cd cerberus_120103_linux
 sleep 40
 ./cerberus-cli stop
 
-echo "Qual o IP da sua VPS ?"
-read ipvps
+ipvps=$(curl -s4 icanhazip.com)
+
+echo "Seu ip é:"
+echo $ipvps
 echo "Qual sua Genkey ?"
 read genkey
 echo "Escreva um nome de usuario !"
