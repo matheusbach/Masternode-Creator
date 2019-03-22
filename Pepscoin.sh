6c82bb8d23e50c95bb1e0bc77d75bd3893acd8a0
ViniciusBitnoob <46408861+ViniciusBitnoob@users.noreply.github.com>
2019-03-22 15:30:35 -0300

    Add files via upload

---
+++ Pepscoin.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+cd
+echo "BEM VINDO AOS SCRYPTS DO CANAL BITNOOB!!!"
+sleep 3 
+echo "Será iniciado a Instalação/Montagem do Masternode de pepscoin v1.0"
+sleep 3
+echo "Instalando Pré-Requisitos..."
+sleep 3 
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install unzip nano -y
+sudo apt-get install git
+sudo apt install rar
+
+
+echo "Limpando arquivos de instalações anteriores..."
+sleep 3
+cd
+rm -r peps
+rm -r .worldcryptoforum
+
+
+echo "Instalando Node..."
+sleep 3
+cd
+mkdir peps
+cd peps
+wget https://github.com/PEPS-Project/peps/releases/download/v2.3-rev2/peps-daemon.zip
+unzip peps-daemon.zip
+./pepsd &
+sleep 20
+./peps-cli stop
+
+ipvps=$(curl -s4 icanhazip.com)
+
+echo "Seu ip é:"
+echo $ipvps
+echo "Qual sua Genkey ?"
+read genkey
+
+echo "rpcuser=pepsrpc" >> ~/.peps/peps.conf
+echo "rpcpassword=8CbcPojumn8i14cK5WzE3EYFAPJK78YtELVUbUHDmTsW" >> ~/.peps/peps.conf
+echo "rpcport=16262" >> ~/.peps/peps.conf
+echo "externalip="$ipvps >> ~/.peps/peps.conf
+echo "masternodeprivkey="$genkey >> ~/.peps/peps.conf
+echo "rpcallowip=127.0.0.1" >> ~/.peps/peps.conf
+echo "listen=1" >> ~/.peps/peps.conf
+echo "server=1" >> ~/.peps/peps.conf
+echo "daemon=1" >> ~/.peps/peps.conf
+echo "masternode=1" >> ~/.peps/peps.conf
+echo "bind="$ipvps >> ~/.peps/peps.conf
+echo "masternodeaddr="$ipvps:15252 >> ~/.peps/peps.conf
+echo "logtimestamps=1" >> ~/.peps/peps.conf
+echo "port=15252" >> ~/.peps/peps.conf
+
+
+echo "Iniciando Masternode..."
+sleep 3
+cd 
+cd peps
+./pepsd &
+sleep 30
+./peps-cli mnsync status
+echo "Se os parametros foram true e a transação na wallet do seu computador ja estiver confirmada, pode iniciar o Masternode"
+echo "Caso não for true, aguarde mais alguns minutos e use os comandos"
+echo "cd"
+echo "cd peps"
+echo "./peps-cli mnsync status"
+echo "Faça isso de tempos em tempos até que note que ja está em true para iniciar o masternode na wallet em seu computador"
+echo "Deverá usar o comando a baixo no console de sua wallet windows para dar start no masternode"
+echo "startmasternode alias false (nomedoalias)"
+echo "AGRADECIMENTOS A DIEGO NEVES DO SITE https://staking.world E A MATHEUS BACH, SEM ELES NAO SERIA POSSIVEL ESTE SCRYPT."
+echo "Att. Vinicius Ferreiro - Canal Bitnoob"
+mv ~/Pepscoin.sh ~/peps
